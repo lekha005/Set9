b71c32af381f33e0a52696c5c46ebdb2d26bda85
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 21:24:06 +0530

    Create Asc.java

---
+++ Asc.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Asc
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+         String a=in.next();
+       int y=Integer.valueOf(a.charAt(0));
+            System.out.print(y);
+    }    
+        
+}
+
+
+
+
+
+
+
+
