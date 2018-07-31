8535447fc0ea087203d072dabe9dde99535ec9dc
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 08:26:25 +0000

    Create Diffi.java

---
+++ Diffi.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Diffi
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+        int r=Math.abs(a-b);
+                System.out.print(r);
+    }    
+        
+}
+
+
+
+
+
+
+
